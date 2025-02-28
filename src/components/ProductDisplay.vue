1e0e9e151be87de2e74b1146a2c78a035cbb79f1
Muhammad Farhan <muhfarhanhidayatulloh@gmail.com>
2025-02-28 10:08:54 +0700

    edit style display component

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -25,16 +25,18 @@ onMounted(() => {
         <img :src="product.image" alt="" />
       </div>
       <div class="product-detail">
-        <h1>{{ product.title }}</h1>
+        <h2 id="product-title">{{ product.title }}</h2>
         <div class="product-info">
           <p>{{ product.category }}</p>
           <p>{{ product.rating.rate }}/5</p>
         </div>
-        <p>{{ product.description }}</p>
-        <h2>{{ product.price }}</h2>
+        <hr />
+        <p id="product-desc">{{ product.description }}</p>
+        <hr />
+        <h2 id="product-price">${{ product.price }}</h2>
         <div class="btn">
-          <button>But now</button>
-          <button>Next Product</button>
+          <button id="btn-buy">But now</button>
+          <button id="btn-next">Next Product</button>
         </div>
       </div>
     </div>
@@ -59,25 +61,61 @@ onMounted(() => {
   padding: 50px 56px;
   background-color: white;
   border-radius: 10px;
-  box-shadow: 0 0 20px #DCDCDC;
+  box-shadow: 2px 4px 21px #dcdcdc;
+}
+#product-title {
+  margin-bottom: 25px;
+  font-size: 28px;
+  color: #720060;
+}
+#product-desc {
+  font-size: 20px;
+  margin-top: 26px;
+  margin-bottom: 62px;
 }
 
+#product-price {
+  margin-top: 16px;
+  margin-bottom: 16px;
+}
 .product-info {
   display: flex;
   justify-content: space-between;
   font-size: 18px;
+  margin-bottom: 12px;
 }
 .image-product {
   width: 400px;
   height: 400px;
   display: flex;
 }
-h1 {
-  color: red;
-  font-size: 50px;
-}
 
 h2 {
-  color: yellow;
+  color: #720060;
+  font-size: 28px;
+}
+.btn {
+  display: flex;
+  gap: 20px;
+  justify-content: space-between;
+  font-weight: 600;
+}
+
+.btn #btn-buy {
+  padding: 10px 80px;
+  font-size: 20px;
+  background-color: #720060;
+  color: white;
+  border-radius: 10px;
+  width: 100%;
+}
+.btn #btn-next {
+  padding: 10px 80px;
+  font-size: 20px;
+  background-color: white;
+  color: #720060;
+  border-radius: 10px;
+  width: 100%;
+  border: 3px solid #720060;
 }
 </style>
