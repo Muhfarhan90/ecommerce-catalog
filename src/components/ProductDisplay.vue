1e51c33c2416e557f407cf5d9452f43c2f0fc582
Muhammad Farhan <muhfarhanhidayatulloh@gmail.com>
2025-02-28 09:08:15 +0700

    refactor code and style code

---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,20 @@
+<script setup>
+import "../assets/style/page.css";
+
+</script>
+
+<template>
+  <h1>Produk display</h1>
+  <h2>Barang satu</h2>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+  font-size: 50px;
+}
+
+h2 {
+  color: yellow;
+}
+</style>
